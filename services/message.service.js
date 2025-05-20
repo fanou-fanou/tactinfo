8232dd2d476cf9e85f197fa90822078ad16ebd66
Fanomezana <rabevasoa.fanomezana@gmail.com>
2025-05-20 15:34:29 +0300

    handle send message form

--- services/message.service.js
+++ services/message.service.js
@@ -1,22 +1,30 @@
-const prisma = require("../config/prisma").default;
+const prisma = require("../config/prisma");
 const { ClientError } = require("../config/error");
 const messageSchema = require("../utils/validators/message.validator");
 
 
 async function saveMessage(data) {
     const { error, value } = messageSchema.validate(data);
+
+
     delete value._csrf;
 
     if (error) {
-
         throw new ClientError({
             text: error.message,
             data: value
         });
     }
 
-    const message = await prisma.messages.create({ ...value });
-    return message ? message : value;
+
+    try {
+        const message = await prisma.messages.create({ data: { ...value } });
+
+        return message ? message : value;
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 module.exports = {
