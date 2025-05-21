bfb1db0b06252aa824f5592aa811a4ef1aa05a10
Fanomezana <rabevasoa.fanomezana@gmail.com>
2025-05-21 13:54:06 +0300

    add resource route om index file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const env = require("./config/env");
 const engine = require("./config/engine");
 const middlewares = require("./config/middlewares/middlewares");
 const errorHandler = require("./config/middlewares/error.middleware");
+const resourcesRoute = require("./routers/resources.route");
 require("./config/passport");
 
 const app = express();
@@ -14,6 +15,7 @@ middlewares.forEach(middleware => app.use(middleware));
 engine(app);
 
 mainRoute.forEach(route => app.use(route.prefix, route.controller));
+resourcesRoute.forEach(route => app.use(`/v1/api${route.prefix}`, route.resource));
 
 app.all("*", function (request, response) {
     return response.render("errors/404.error.njk");
